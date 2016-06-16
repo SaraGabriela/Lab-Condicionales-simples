89dc6c3f339cd17744475cb9e57170abe9d210cd
Sara Gabriela Díaz Oporto <sara.diaz.oporto@gmail.com>
2016-06-16 14:13:24 -0500

    bla

--- Ejercicio 1 APROBADO O REPROBADO/Problema/main.js
+++ Ejercicio 1 APROBADO O REPROBADO/Problema/main.js
@@ -12,7 +12,6 @@ var n1=parseFloat(prompt("Ingrese su primera nota"));
 var nota1=valida(n1);
 var n2=parseFloat(prompt("Ingrese su segunda nota"));
 var nota2=valida(n2);
-alert(nota2);
 var n3=parseFloat(prompt("Ingrese su tercera nota"));
 var nota3=valida(n3);
 var promedio=(nota1+nota2+nota3)/3;
